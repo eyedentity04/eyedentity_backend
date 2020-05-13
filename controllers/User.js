b3c0f663137f521294858bd38b6a21c63117f7e7
sony-nurdianto <sonynurdianto445@gmail.com>
2020-05-13 16:53:36 +0700

    add Bearer

--- controllers/User.js
+++ controllers/User.js
@@ -80,7 +80,7 @@ module.exports = {
                       }, (err,token) => {
                           res.json({
                               success : true,
-                              token : token,
+                              token : 'Bearer' + token,
                               id : user.id
                           })
                       })

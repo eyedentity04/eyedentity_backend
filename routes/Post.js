82f16a85dde8c8328779fef8896705743e304c05
sony-nurdianto <sonynurdianto445@gmail.com>
2020-05-14 19:11:41 +0700

    test images

--- routes/Post.js
+++ routes/Post.js
@@ -5,7 +5,7 @@ const multer = require ("multer")
 
 const storage = multer.diskStorage({
     destination: function (req,res,cb){
-        cb(null,"./public")
+        cb(null, "./public/images" )
     },
     filename: function(req,file,cb){
         cb(null, new Date().toISOString()+file.originalname)
